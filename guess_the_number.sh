71269c83003a94124d73ac93d8a88b30d1f70887
talandar99 <talandar66@protonmail.com>
2023-10-26 13:38:13 +0200

    feat: guess_the_number is finnished

--- guess_the_number.sh
+++ guess_the_number.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-
 main (){
     NUMBER=$(($RANDOM%100))
-    echo $NUMBER
-    echo "Guess the number"
-    read USER_NUMBER
-    echo $USER_NUMBER
+    USER_NUMBER=-1
+    while [ $NUMBER -ne $USER_NUMBER ]; 
+    do 
+        guess
+        feedback
+    done
+    victory
 }
 
+guess (){
+    echo "Guess the number"
+    read USER_NUMBER
+}
+
+feedback (){
+    if [ $USER_NUMBER -lt $NUMBER ] 
+    then
+        echo "your number is smaller than mine"
+    else 
+        echo "your number is bigger than mine"
+    fi
+}
+
+victory (){
+    echo "-----------------------------------------"
+    echo "Congratulations"
+    echo "My number is"
+    echo $NUMBER
+}
 main
+
